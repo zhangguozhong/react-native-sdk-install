69c088990ce9d1da718407657d6f3e035aafbf6a
zhangguozhong <guozhong.zhang@mobilemd.cn>
2019-03-18 20:44:20 +0800

    js view

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-
-import { NativeModules } from 'react-native';
-
+import ProgressBarModal from './views/ProgressBarModal';
+import { NativeModules,DeviceEventEmitter,Platform } from 'react-native';
 const { RNSdkInstall } = NativeModules;
 
-export default RNSdkInstall;
+export default class InstallSDK {
+
+    static downloadAndInstall(url,show,hide,update) {
+        if (Platform.OS === 'android') {
+            this.addListener('progressWillShow',show);
+            this.addListener('progressWillHide',hide);
+            this.addListener('progressWillUpdate',update);
+            RNSdkInstall.downloadAndInstall(url);
+        }
+    }
+
+    static addListener(eventName,callback) {
+        DeviceEventEmitter.addListener(eventName, (event) => {
+            excuteCallback(callback,event);
+        });
+    }
+};
+
+function excuteCallback(callback,params) {
+    callback && callback(params);
+}
+
+export { ProgressBarModal };
