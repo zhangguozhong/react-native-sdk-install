a331634d83d06b26ddeb2709c639f305fd11d44c
zhangguozhong <guozhong.zhang@mobilemd.cn>
2019-03-18 20:23:55 +0800

    download

---
+++ android/src/main/java/com/apk/install/DownloadAndInstall.java
@@ -0,0 +1,98 @@
+package com.apk.install;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.annotation.Nullable;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import static com.facebook.react.bridge.UiThreadUtil.runOnUiThread;
+
+public class DownloadAndInstall {
+
+    public static void start(final String url, final Activity activity, final ReactApplicationContext reactContext) {
+        if (url == null) {
+            return;
+        }
+        sendEvent(reactContext,"progressWillShow",null); //弹出进度条对话框
+
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    File file = getFileFromServer(url,reactContext);
+                    //安装APK
+                    installApk(file,activity);
+                    sendEvent(reactContext,"progressWillHide",null); //结束掉进度条对话框
+                }catch (Exception e) {
+                }
+            }}.start();
+    }
+
+    private static File getFileFromServer(String path, final ReactApplicationContext reactContext) throws Exception {
+        //如果相等的话表示当前的sdcard挂载在手机上并且是可用的
+        if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+            URL url = new URL(path);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(5000);
+
+            //获取到文件的大小
+            int contentLength = conn.getContentLength();
+            InputStream is = conn.getInputStream();
+            File file = new File(Environment.getExternalStorageDirectory(), "app-release.apk");
+            FileOutputStream fos = new FileOutputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(is);
+            byte[] buffer = new byte[1024];
+            int len ;
+            int currentLength = 0;
+            while((len = bis.read(buffer)) != -1){
+                fos.write(buffer, 0, len);
+                currentLength += len;
+                final int progress = currentLength / contentLength;
+
+                //获取当前下载量
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        WritableMap params = Arguments.createMap();
+                        params.putDouble("progress",progress);
+                        sendEvent(reactContext,"progressWillUpdate",params);
+                    }
+                });
+            }
+            fos.close();
+            bis.close();
+            is.close();
+            return file;
+        }
+        else{
+            return null;
+        }
+    }
+
+    private static void sendEvent(ReactContext reactContext,
+                           String eventName,
+                           @Nullable WritableMap params) {
+        reactContext
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
+    }
+
+    private static void installApk(File file, Activity activity) {
+        Intent intent = new Intent();
+        //执行动作
+        intent.setAction(Intent.ACTION_VIEW);
+        //执行的数据类型
+        intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+        activity.startActivity(intent);
+    }
+}
--- android/src/main/java/com/apk/install/RNSdkInstallModule.java
+++ android/src/main/java/com/apk/install/RNSdkInstallModule.java
@@ -1,10 +1,12 @@
 
 package com.apk.install;
 
+import android.Manifest;
+import android.widget.Toast;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 
 public class RNSdkInstallModule extends ReactContextBaseJavaModule {
 
@@ -18,5 +20,19 @@ public class RNSdkInstallModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNSdkInstall";
+  }
+
+  @ReactMethod
+  public void downloadAndInstall(final String url) {
+    PermissionUtils.permissionsCheck(getCurrentActivity(),new String[]{ Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE },new PermissionUtils.OnPermissionListener() {
+      @Override
+      public void onPermissionGranted() {
+        DownloadAndInstall.start(url, getCurrentActivity(),getReactApplicationContext());
+      }
+      @Override
+      public void onPermissionDenied(String[] deniedPermissions) {
+        Toast.makeText(getCurrentActivity(),"读写权限不够,无法下载！",Toast.LENGTH_SHORT).show();
+      }
+    });
   }
 }
